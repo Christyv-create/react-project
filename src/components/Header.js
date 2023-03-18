407bcc336b8dd558f4f9d183029059a52156889f
christyv-create <christyvaskovics@gmail.com>
2023-03-17 20:43:31 -0400

    second commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const header = () => {
+  return (
+    <header><h1>Budy's Dog Walking Service</h1>
+        </header>
+
+          
+  )
+}
+
+export default header
+
